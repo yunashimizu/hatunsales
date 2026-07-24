1844407cfd0d40a51a15ae34ecf57a1c323f2ef8
yuta <kenymarallana132@gmail.com>
2026-07-24 11:55:36 -0500

    ferreteria

--- src/config/nubefact.config.ts
+++ src/config/nubefact.config.ts
@@ -1,4 +1,4 @@
 export const nubefactConfig = {
   url: process.env.NUBEFACT_URL ?? 'https://api.nubefact.com/api/v1/36c156cd-38a4-4f9f-ac70-a5b8b545fc08',
-  token: process.env.NUBEFACT_TOKEN ?? '',
+  token: process.env.NUBEFACT_TOKEN ?? '62103c60f724481c91cec368aaf0a55b030857ce722a40ee8889c2bd64d04d48',
 };
